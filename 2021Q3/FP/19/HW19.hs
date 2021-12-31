bf81fcec468249c573eda350a7ca6d9891fcc125
thezzisu <thezzisu@gmail.com>
2021-12-31 18:35:03 +0800

    chore: update

--- 2021Q3/FP/19/HW19.hs
+++ 2021Q3/FP/19/HW19.hs
@@ -2,4 +2,7 @@ module HW19 where
 
 -- BMF3-2
 tailsums :: Num a => [a] -> [a]
-tailsums = reverse . scanr (+) 0
+tailsums = fst . foldr (\x (xs, s) -> (xs ++ [x + s], x + s)) ([0], 0)
+
+-- Anyway, the following will probably be faster:
+-- tailsums = reverse . scanr (+) 0
