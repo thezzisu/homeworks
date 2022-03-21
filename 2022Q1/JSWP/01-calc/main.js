f4d41003a14fbc09ad29ddc0482b4962b647bec6
thezzisu <thezzisu@gmail.com>
2022-03-21 09:46:56 +0800

    chore: update

---
+++ 2022Q1/JSWP/01-calc/main.js
@@ -0,0 +1,29 @@
+/** @type {HTMLInputElement} */
+const display = document.querySelector(".display");
+
+const SPECIAL = {
+  C: () => (display.value = ""),
+  "⬅": () =>
+    (display.value = display.value.substring(0, display.value.length - 1)),
+  "=": () => (display.value = eval(display.value)),
+  "÷": "/",
+  "×": "*",
+};
+
+const buttons = document.querySelectorAll(".calc-btn");
+for (const button of buttons) {
+  const op = button.textContent.trim();
+  if (op in SPECIAL) {
+    const alt = SPECIAL[op];
+    switch (typeof alt) {
+      case "string":
+        button.addEventListener("click", () => (display.value += alt));
+        break;
+      case "function":
+        button.addEventListener("click", alt);
+        break;
+    }
+  } else {
+    button.addEventListener("click", () => (display.value += op));
+  }
+}
